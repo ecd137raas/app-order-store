7aee05564f3b916c598fd485288aeb83a1e8f17f
Emerson Duarte <raasjakarta@gmail.com>
2024-02-06 20:50:20 -0300

    time for order page

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const http = require('http');
 const app = require('./app')
 
-app.listen('3000')
+const port = 3000; // ou a porta que você está usando
+
+app.listen(port, '192.168.1.4', () => {
+  console.log(`Server running at http://192.168.1.4:${port}/`);
+});
