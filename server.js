6d0f222db5c902e117fcce39acf88f3a9fdab5e7
Emerson Duarte <raasjakarta@gmail.com>
2024-02-19 22:37:52 -0300

    time for order page

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const http = require('http');
 const app = require('./app')
+require('dotenv').config()
 
-const port = 3000; // ou a porta que você está usando
+const port = process.env.PORT || 3333
 
-app.listen(port, '192.168.1.4', () => {
-  console.log(`Server running at http://192.168.1.4:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
--- src/infra/database.js
+++ src/infra/database.js
@@ -1,12 +1,11 @@
+require('dotenv').config()
 const postgree = require('pg-promise')()
 
 const db = postgree({
-    user: 'postgres',
-    password: '1234',
-    host: 'localhost',
-    port: '5432',
-    database: 'postgres'
-
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DATABASE
 })
-
 module.exports = db;
